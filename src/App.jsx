318de03994d42b9819ec5abb1978ab1413cd7edb
Danielle de Avila <danivandinha1@gmail.com>
2024-02-05 10:40:38 -0300

    meu contador

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css"
+import "./index.css"
+import Contador from "./Contador/Contador.jsx"
+
+function App(){
+    return(
+        <>
+        <Contador/>
+        </>
+    )
+}
+export default App
